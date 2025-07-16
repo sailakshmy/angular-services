740116a1dbdff2a50bd469e7fbcec6f3fb7c218f
Sai <sailakshmy94@gmail.com>
2025-07-16 09:11:38 +0400

    Used the inject() which is the alternative dependency injection

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -5,7 +5,8 @@ import { Task } from "./tasks/task.model";
   providedIn: "root",
 })
 export class TasksService {
-  tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
 
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
